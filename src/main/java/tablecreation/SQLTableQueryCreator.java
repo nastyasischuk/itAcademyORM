1bfba06702fd4b29c49a0cbf691386402996e30d
Djuba <Djubabagdad@gmail.com>
2019-09-24 22:14:47 +0300

    SQL TableQueryCreator updated

--- src/main/java/tablecreation/SQLTableQueryCreator.java
+++ src/main/java/tablecreation/SQLTableQueryCreator.java
@@ -1,54 +1,92 @@
 package tablecreation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SQLTableQueryCreator {
-    private ForeignKey fk;
     private Table table;
 
+    public SQLTableQueryCreator(Table table) {
+        this.table = table;
+    }
 
-    String createTableQuery(){
+    public String createTableQuery() {
         StringBuilder request = new StringBuilder();
-        request.append(SQLStatements.CREATETABLE).append(fk.getTableName()).append("\n");
-        for(Column column: table.getColumns()){
-            request.append(column.getName()).append(" ").append(column.getType().toString()).append("\n");
-            if(column.isNullable()){
+        request.append(SQLStatements.CREATE_TABLE).append(table.getTableName()).append('(');
+        for (Column column : table.getColumns()) {//TODO value of column.getType length varchar
+            request.append(column.getName()).append(" ").append(column.getType().toString());
+            if (!column.isNullable()) {
                 request.append(SQLStatements.NOT_NULL);
-            }if(column.isUnique()){
+            }
+            if (column.isUnique()) {
                 request.append(SQLStatements.UNIQUE);
-            }if(column.isAutoincrement()){
+            }
+            if (column.isAutoincrement()) {
                 request.append(SQLStatements.A_INCREMENT);
+            }if(!(table.getCheckConstraint().equals(""))){
+                request.append(SQLStatements.CHECK).append('(').append(table.getCheckConstraint()).append(')');
             }
         }
+        request.append(')').append(';');
         return request.toString();
     }
 
-    public String createFKQuery(){
+    public List<String> createIndexQuery() {
+        List<String> queryIndexList = new ArrayList<>();
         StringBuilder request = new StringBuilder();
-        request.append(SQLStatements.ALTER).append(fk.getTableName()).append("\n")
-                .append(SQLStatements.ADD).append(SQLStatements.CONSTRAINT).append(fk.getConstructionName()).append("\n")
-                .append(SQLStatements.FK).append('(').append(fk.getForeignKeyName()).append(')')
-                .append(SQLStatements.REFERENCE).append(fk.getReferenceTableName())
-                .append('(').append(fk.getReferencePKName()).append(')');
-        return request.toString();
+        StringBuilder columns = new StringBuilder();
+        request.append(SQLStatements.CREATE);
+        for (Index index: table.getIndexes()){
+            if(index.isUnique()){
+                request.append(SQLStatements.UNIQUE);
+            }
+            request.append(SQLStatements.INDEX).append(index.getName()).append(SQLStatements.ON);
+            for(Column column: index.getColumnsInIndex()){
+                columns.append(column);
+                if(index.getColumnsInIndex().isEmpty()){
+                    break;
+                }else{
+                    columns.append(", ");
+                }
+            }
+            request.append('(').append(columns).append(')').append(';');
+            queryIndexList.add(request.toString());
+        }
+        return queryIndexList;
     }
 
-    public String createPKQuery(){
-        PrimaryKey pk = new PrimaryKey();
+    public List<String> createFKQuery() {//TODO Handle empty of foreign keys
+        List<String> queryFKList = new ArrayList<>();
+        StringBuilder request = new StringBuilder();
+        for (ForeignKey fk : table.getForeignKeys())
+            if (!table.getForeignKeys().isEmpty()) {
+                request.append(SQLStatements.ALTER_TABLE).append(fk.getTableName())
+                        .append(SQLStatements.ADD).append(SQLStatements.CONSTRAINT).append(fk.getConstructionName())
+                        .append(SQLStatements.FK).append('(').append(fk.getForeignKeyName()).append(')')
+                        .append(SQLStatements.REFERENCE).append(fk.getReferenceTableName())
+                        .append('(').append(fk.getReferencePKName()).append(')').append(';');
+                queryFKList.add(request.toString());
+            }
+        return queryFKList;
+    }
+
+    public String createPKQuery() {
         StringBuilder request = new StringBuilder();
         StringBuilder columnNames = new StringBuilder();
-        request.append(SQLStatements.ALTER).append(fk.getTableName()).append("\n")
+        request.append(SQLStatements.ALTER_TABLE).append(table.getTableName())
                 .append(SQLStatements.ADD);
-        if(pk.getPKList().size() > 1){
-            request.append(SQLStatements.CONSTRAINT).append("default");
+        if (table.getPrimaryKey().getPKList().size() > 1) {
+            request.append(SQLStatements.CONSTRAINT).append("Need to create method constraint ");
         }
-        for (Column column: pk.getPKList()){
+        for (Column column : table.getPrimaryKey().getPKList()) {
             columnNames.append(column.getName());
-            if(pk.getPKList().isEmpty()){
+            if (table.getPrimaryKey().getPKList().isEmpty()) {
                 break;
-            }else{
+            } else {
                 columnNames.append(", ");
             }
         }
-                request.append(SQLStatements.PK).append('(').append(columnNames).append(')');
+        request.append(SQLStatements.PK).append('(').append(columnNames).append(')').append(';');
         return request.toString();
     }
 
