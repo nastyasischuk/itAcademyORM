5515cabe69ff5a48810101bd2660a18f797839d7
Nastya <nastyasischuk@gmail.com>
2019-09-27 14:16:16 +0300

    changed method getNameOfField

--- src/main/java/CRUD/rowhandler/RowConstructor.java
+++ src/main/java/CRUD/rowhandler/RowConstructor.java
@@ -1,6 +1,6 @@
 package CRUD.rowhandler;
 
-import annotations.Column;
+import annotations.*;
 
 import java.lang.reflect.Field;
 
@@ -10,14 +10,30 @@ public abstract class RowConstructor  {
         if (classToConvertTorow.isAnnotationPresent(annotations.Table.class) && !classToConvertTorow.getAnnotation(annotations.Table.class).name().equals("")) {
             return classToConvertTorow.getAnnotation(annotations.Table.class).name();
         } else {
-            return classToConvertTorow.getClass().getSimpleName();
+            return classToConvertTorow.getSimpleName();
         }
 
     }
     protected String getNameOfField(Field field){
-        if(field.isAnnotationPresent(Column.class) && !field.getAnnotation(Column.class).name().equals("")){
-            return field.getAnnotation(Column.class).name();
+        if(field.isAnnotationPresent(annotations.Column.class) && !field.getAnnotation(annotations.Column.class).name().equals("")){
+        return field.getAnnotation(annotations.Column.class).name();
+    }else if(field.isAnnotationPresent(ForeignKey.class) && !field.getAnnotation(ForeignKey.class).name().equals("")){
+        return field.getAnnotation(ForeignKey.class).name();
+    }else if (field.isAnnotationPresent(MapsId.class) && field.isAnnotationPresent(OneToOne.class)) {
+        Class currentClass = field.getDeclaringClass();
+        Field[] fields = currentClass.getDeclaredFields();
+        for (Field f : fields) {
+            if (f.isAnnotationPresent(PrimaryKey.class)) {
+                if (f.isAnnotationPresent(annotations.Column.class) && !f.getAnnotation(annotations.Column.class).name().equals("")) {
+                    return f.getAnnotation(annotations.Column.class).name();
+                } else {
+                    return f.getName();
+                }
+            }
         }
         return field.getName();
+    }else{
+        return field.getName();
+    }
     }
 }
