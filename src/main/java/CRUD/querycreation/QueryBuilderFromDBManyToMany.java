a9b5fc5d5e619fa40f1c94cb03a52262b32310af
Djuba <Djubabagdad@gmail.com>
2019-10-02 22:55:40 +0300

    query for ManyToMany

--- src/main/java/CRUD/querycreation/QueryBuilderFromDBManyToMany.java
+++ src/main/java/CRUD/querycreation/QueryBuilderFromDBManyToMany.java
@@ -2,9 +2,12 @@ package CRUD.querycreation;
 
 import CRUD.requests.MarkingChars;
 import CRUD.rowhandler.RowFromDB;
+import CRUD.rowhandler.RowFromDBManyToMany;
+import CRUD.rowhandler.RowToDB;
 import tablecreation.SQLStatements;
 
 public class QueryBuilderFromDBManyToMany {
+    RowFromDBManyToMany row;
     RowFromDB rowFromDB;
     String nameJoinTable;
 
@@ -14,13 +17,13 @@ public class QueryBuilderFromDBManyToMany {
     }
     public String buildQuery(){
         StringBuilder query = new StringBuilder();
-        query.append(SQLStatements.SELECT.getValue()).append(MarkingChars.star).append(SQLStatements.FROM.getValue())
-                .append(rowFromDB.getTableName()).append(SQLStatements.JOIN.getValue()).append(nameJoinTable)
-                .append(SQLStatements.ON.getValue()).append(rowFromDB.getTableName()).append(MarkingChars.dot).append(rowFromDB.getIdName())
-                .append(MarkingChars.equally).append(nameJoinTable).append(MarkingChars.dot).append(rowFromDB.getIdName())
-                .append(SQLStatements.INNER.getValue()).append(SQLStatements.JOIN.getValue()).append(SQLStatements.WHERE.getValue())
-                .append(nameJoinTable).append(MarkingChars.dot).append(rowFromDB.getIdName()).append(MarkingChars.equally)
-                .append(rowFromDB.getIdValue()).append(MarkingChars.semicolon);
+        query.append(SQLStatements.SELECT.getValue()).append(rowFromDB.getTableName()).append(MarkingChars.dot).append(rowFromDB.getIdName())
+                .append(MarkingChars.comma).append(row.getTableName()).append(row.getInverse_column()).append(row.getJoin_coulmn())
+                .append(SQLStatements.FROM.getValue()).append(rowFromDB.getTableName()).append(SQLStatements.INNER).append(SQLStatements.JOIN.getValue())
+                .append(row.getManyToManyTableName()).append(SQLStatements.ON.getValue()).append(rowFromDB.getTableName()).append(MarkingChars.dot)
+                .append(rowFromDB.getIdName()).append(MarkingChars.equally).append(row.getManyToManyTableName()).append(MarkingChars.dot)
+                .append(row.getInverse_column()).append(SQLStatements.WHERE.getValue()).append(row.getManyToManyTableName()).append(MarkingChars.dot)
+                .append(row.getJoin_coulmn()).append(MarkingChars.equally).append(rowFromDB.getIdValue()).append(MarkingChars.semicolon);
         return query.toString();//example of query to build
         // select * from tablename from row
        // join nameJoinTable p on cat.id = p.c_id
