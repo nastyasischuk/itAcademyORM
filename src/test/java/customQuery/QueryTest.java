768ff5eabc29e3cd421bde69aaf58bbfb7d800fa
Nastya <nastyasischuk@gmail.com>
2019-10-07 13:07:28 +0300

    query test added close

--- src/test/java/customQuery/QueryTest.java
+++ src/test/java/customQuery/QueryTest.java
@@ -19,24 +19,27 @@ public class QueryTest {
     @BeforeClass
     public static void setDataBase() {
         dataBase = new DataBaseImplementation("D:\\my files\\softserve\\itAcademyORM\\src\\main\\resources\\config.xml", "catspeople", false);
-    dataBase.openConnection();
+
     }
     @Test
     public void getSingleObject() {
+        dataBase.openConnection();
         QueryBuilderImpl query = new QueryBuilderImpl(PersonOneToMany.class,dataBase);
         query.select().where(query.getLimits().equals("id", "1")).fetch();
         QueryResult<PersonOneToMany> custQuery = new QueryResult<>(query);
         PersonOneToMany person = custQuery.getSingleObject();
         Assert.assertEquals(1,person.getId());
+        dataBase.close();
     }
 
     @Test
     public void getListOfFoundObjects() {
+        dataBase.openConnection();
         QueryBuilderImpl query = new QueryBuilderImpl(PersonOneToMany.class,dataBase);
         query.select().fetch();
-
         QueryResult<PersonOneToMany> custQuery = new QueryResult<>(query);
         List<PersonOneToMany> person = custQuery.getListOfFoundObjects();
         Assert.assertEquals(4,person.size());
+        dataBase.close();
     }
 }
